f1dc079b5366896e939c6e3ac15b70e7100be4a4
NganVanLuyen <nganvanluyen@gmail.com>
2021-05-22 23:08:04 +0700

    first commit

---
+++ app/src/main/java/com/example/doodlzapp/EraseImageDialogFragment.java
@@ -0,0 +1,66 @@
+package com.example.doodlzapp;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+
+public class EraseImageDialogFragment extends DialogFragment {
+
+    // create an AlertDialog and return it
+    @Override
+    public Dialog onCreateDialog(Bundle bundle) {
+        AlertDialog.Builder builder =
+                new AlertDialog.Builder(getActivity());
+
+        // set the AlertDialog's message
+        builder.setMessage(R.string.message_erase);
+
+        // add Erase Button
+        builder.setPositiveButton(R.string.button_erase,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        getDoodleFragment().getDoodleView().clear(); // clear image
+                    }
+                }
+        );
+
+        // add cancel Button
+        builder.setNegativeButton(android.R.string.cancel, null);
+        return builder.create(); // return dialog
+    }
+
+    // gets a reference to the MainActivityFragment
+    private MainActivityFragment getDoodleFragment() {
+        return (MainActivityFragment) getFragmentManager().findFragmentById(
+                R.id.doodleFragment);
+    }
+
+    // tell MainActivityFragment that dialog is now displayed
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        MainActivityFragment fragment = getDoodleFragment();
+
+        if (fragment != null)
+            fragment.setDialogOnScreen(true);
+    }
+
+    // tell MainActivityFragment that dialog is no longer displayed
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        MainActivityFragment fragment = getDoodleFragment();
+
+        if (fragment != null)
+            fragment.setDialogOnScreen(false);
+    }
+
+    public void show(FragmentManager fragmentManager, String erase_dialog) {
+    }
+}
---
+++ app/src/main/java/com/example/doodlzapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.doodlzapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+        // determine screen size
+        int screenSize =
+                getResources().getConfiguration().screenLayout &
+                        Configuration.SCREENLAYOUT_SIZE_MASK;
+
+        // use landscape for extra large tablets; otherwise, use portrait
+        if (screenSize == Configuration.SCREENLAYOUT_SIZE_XLARGE)
+            setRequestedOrientation(
+                    ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        else
+            setRequestedOrientation(
+                    ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+}
